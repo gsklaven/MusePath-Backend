9c9b4932aebdc6ee7efbbd2a517615127bc1b199
giorgosskl <sklavenitisgeorge21@gmail.com>
2025-12-19 19:10:58 +0200

    lower people

--- tests/k6/health_load.js
+++ tests/k6/health_load.js
@@ -1,7 +1,10 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 export const options = {
-  stages: [{ duration: '30s', target: 20 }, { duration: '1m', target: 20 }, { duration: '30s', target: 0 }],
+  stages: [
+    { duration: '20s', target: 10 }, 
+    { duration: '40s', target: 10 }, 
+    { duration: '20s', target: 0 }],
   thresholds: { http_req_duration: ['p(95)<500'], http_req_failed: ['rate<0.01'] },
 };
 export default function () {
