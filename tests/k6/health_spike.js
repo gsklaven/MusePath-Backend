ccd80ca314af2bf6ce289ff44736b15f9b4a3f5b
giorgosskl <sklavenitisgeorge21@gmail.com>
2025-12-19 19:29:32 +0200

    lower more spike people

--- tests/k6/health_spike.js
+++ tests/k6/health_spike.js
@@ -1,8 +1,8 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 export const options = {
-  stages: [{ duration: '10s', target: 10 }, 
-    { duration: '20s', target: 10 }, 
+  stages: [{ duration: '10s', target: 5 }, 
+    { duration: '20s', target: 5 }, 
     { duration: '10s', target: 0 }],
   thresholds: { http_req_duration: ['p(95)<1000'], http_req_failed: ['rate<0.05'] },
 };
