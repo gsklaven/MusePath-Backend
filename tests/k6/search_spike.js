0838e3fa00888c7ee059cd9586d3120f5a319b9f
giorgosskl <sklavenitisgeorge21@gmail.com>
2025-12-30 20:31:20 +0200

    users

--- tests/k6/search_spike.js
+++ tests/k6/search_spike.js
@@ -5,7 +5,7 @@ export const options = {
   stages: [
     { duration: '10s', target: 10 },  // Προθέρμανση
     { duration: '30s', target: 500 }, // Spike στους 500 χρήστες
-    { duration: '1m', target: 800 },  // Διατήρηση πίεσης
+    { duration: '1m', target: 500 },  // Διατήρηση πίεσης
     { duration: '20s', target: 10 },  // Αποκλιμάκωση
     { duration: '10s', target: 0 },
   ],
